a871ee1fefaab379a72c79451be8f98e3dd2cc79
pc <shahidakber2020@cs.ajce.in>
2021-03-23 13:33:59 +0530

    today

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { LoginComponent } from '../login/login.component';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
 @Component({
@@ -8,14 +9,43 @@ import { DataService } from '../services/data.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  acno="username"
- // private login:LoginComponent
-  constructor() { }
+  acno = "";
+  pwd = "";
+  amt = "";
+  depositForm = this.fb.group({
+    acno: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(5), Validators.pattern('[0-9]*')]],
+    pwd: ['', [Validators.required, Validators.minLength(4), Validators.pattern('[A-Z a-z 0-9]*')]],
+    amt: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(7), Validators.pattern('[0-9]*')]]
+  });
+  withdrawForm = this.fb.group({
+    acno: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(5), Validators.pattern('[0-9]*')]],
+    pwd: ['', [Validators.required, Validators.minLength(4), Validators.pattern('[A-Z a-z 0-9]*')]],
+    amt: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(7), Validators.pattern('[0-9]*')]]
+  });
+
+
+  constructor(private router: Router, public dataService: DataService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
-  //acno=this.login.accno
-  
+  deposit() {
+    if (this.depositForm.valid) {
+      var result = this.dataService.deposit(this.depositForm.value.acno, this.depositForm.value.pwd, this.depositForm.value.amt)
+
+    }
+    else {
+      alert("Invalid Form")
+    }
+  }
+  withdraw() {
+    if (this.withdrawForm.valid) {
+      var result = this.dataService.withdraw(this.withdrawForm.value.acno, this.withdrawForm.value.pwd, this.withdrawForm.value.amt)
+
+    }
+    else {
+      alert("Invalid Form")
+    }
+  }
 
 
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -4,8 +4,12 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class DataService {
+  currentUser: any;
+  constructor() { 
+   this.getDetails()
 
-  constructor() { }
+
+  }
   accountDetails: any = {
     1000: { acno: 1000, name: "userone", balance: 5000, password: "user1" },
     1002: { acno: 1002, name: "usertwo", balance: 3000, password: "user2" },
@@ -13,6 +17,18 @@ export class DataService {
     1004: { acno: 1004, name: "userfour", balance: 3000, password: "user4" },
     1005: { acno: 1005, name: "userfive", balance: 3000, password: "user5" }
   }
+  saveDetails(){
+    localStorage.setItem("Accountdetails",JSON.stringify(this.accountDetails))
+    localStorage.setItem("CurrentUser",JSON.stringify(this.currentUser))
+  }
+  getDetails()
+  {
+    this.accountDetails=localStorage.getItem("Accountdetails")
+    this.currentUser=localStorage.getItem("CurrentUser")
+  }
+
+
+
   register(acno: any, password: any) {
     if (acno in this.accountDetails) {
       alert("user exist")
@@ -24,9 +40,72 @@ export class DataService {
         balance: 0,
         password
       }
+      this.saveDetails();
       alert("Registration sucessful")
       console.log(this.accountDetails);
       return true;
     }
+  }
+  deposit(acno: any, pwd: any, amt: any) {
+    var amount = parseInt(amt)
+    let dataset = this.accountDetails;
+    if (acno in dataset) {
+      var pswd1 = dataset[acno].password
+
+      if (pswd1 == pwd) {
+        dataset[acno].balance += amount;
+        alert("Account credited with amount: " + amount + " New balance is: " + dataset[acno].balance)
+      }
+      else {
+        alert("Invalid Password")
+      }
+    }
+    else {
+      alert("INVALID DATA")
+    }
+
+  }
+  login(accno: any, pwd: any) {
+    let dataset = this.accountDetails;
+    if (accno in dataset) {
+      var pswd1 = dataset[accno].password
+      this.currentUser = dataset[accno].name
+      if (pswd1 == pwd) {
+        alert("Login successful")
+        return true
+      }
+      else {
+        alert("Invalid Password")
+        return false
+      }
+    }
+    else {
+      alert("INVALID DATA")
+      return false
+    }
+  }
+  withdraw(acno: any, pwd: any, amt: any) {
+    var amount = parseInt(amt)
+    let dataset = this.accountDetails;
+    if (acno in dataset) {
+      var pswd1 = dataset[acno].password
+
+      if (pswd1 == pwd) {
+        if (amount > dataset[acno].balance) {
+          alert("Insufficient Balance")
+        }
+        else {
+          dataset[acno].balance -= amount;
+          alert("Account debited with amount: " + amount + " New balance is: " + dataset[acno].balance)
+        }
+      }
+      else {
+        alert("Invalid Password")
+      }
+    }
+    else {
+      alert("INVALID DATA")
+    }
+
   }
 }
